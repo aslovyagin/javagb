8d7744b31e23fef82b5ee51557b21425d9eeea2b
aleksey <aleksey.slovyagin95@gmail.com>
2021-03-24 18:56:37 +0300

    lesson 3 task 3

--- lesson3/src/main/java/Main.java
+++ lesson3/src/main/java/Main.java
@@ -4,6 +4,26 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static void guessNumber() {
+        int flag = 1;
+        while (flag == 1) {
+            int number = (int) (Math.random() * 9);
+            System.out.println(number);
+            for (int i = 0; i < 3; i++) {
+                int userNumber = new Scanner(System.in).nextInt();
+                if (userNumber == number) {
+                    break;
+                } else {
+                    System.out.println(userNumber > number ? "Введите число меньше" : "Введите число больше");
+                }
+            }
+            System.out.println("Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет)");
+            flag = new Scanner(System.in).nextInt();
+
+        }
+
+    }
+
     public static void guessWord() {
         String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
         String word = words[(int) (Math.random() * words.length)];
@@ -33,6 +53,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        guessWord();
+        guessNumber();
     }
 }
